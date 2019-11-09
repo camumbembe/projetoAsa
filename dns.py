791f3b09bbf796f66fa2d55e8b5afde9334681d8
camumbembe <rafaellamgsouza@gmail.com>
2019-11-09 12:34:23 -0300

    funcionando

--- dns.py
+++ dns.py
@@ -5,26 +5,35 @@ nomeZona = input('Informe o nome do seu domínio:')
 
 arquivo.write(“zone nomeZona IN {”) 
 arquivo.write(“type master;”) 
-arquivo.write(“file nomeZona+'.zone' ;”) 
-arquivo.write(“};”) 
- 
-arquivo.close() 
+arquivo = open('teste.txt', 'a+')
 
-arqZone = open('/var/named/projeto/nomedazona', 'a+')
+nomeZona = input('Informe o nome do seu domínio:')
 
-arqZone.write("$TTL 10")
-arqZone.write("$ORIGIN nomeZona+'.'")
-arqZone.write("@                        IN SOA       @ root (")
-arqZone.write("10;")
-arqZone.write("3H;")
-arqZone.write("15M;")
-arqZone.write("1W;")
-arqZone.write("10);")
-arqZone.write("                        IN NS    @")
-arqZone.write("                        IN A         192.168.102.156")
-arqZone.write("www             IN      A       192.168.102.156")
-arqZone.write("")
-arqZone.write("")
-arqZone.write("")
 
+
+arquivo.write( """zone "{0}" IN {{
+type master;
+file "{0}.zone";  
+}};""".format(nomeZona))
+
+#arquivo.write(teste.format(nomeZona))
+
+arquivo.close()
+
+arqZone = open('teste2.txt', 'a+')
+
+arqZone.write(""" $TTL 10
+   $ORIGIN nomeZona.
+   @                        IN SOA       @ root (
+   10;
+   3H;
+   15M;
+   1W;
+            10);
+      IN NS    @
+      IN A         192.168.102.156
+www             IN      A       192.168.102.156
+""")
+
+arqZone.close()
 
